56d0c4ca6f482012f39deb81a9d99f5f2686e0f6
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-14 19:47:04 -0300

    081424 aula0303

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+//recuperar valores. nome, quantidade e valor
+let nomeProduto = document.getElementById('produto');
+let quantidade = document.getElementById('quatidade');
+alert(nomeProduto.value);
+alert(quantidade.value);
+
+//calcular o preço, subtotal
+//adicionar no carrinho e subtotal
+//atualizar o valor total
+}
+
+function limpar() {
+
+}
