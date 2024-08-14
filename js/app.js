e05de09d30731c09cc637438c8f510fe4446a667
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-14 20:00:29 -0300

    081424 aula0304

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 function adicionar() {
 //recuperar valores. nome, quantidade e valor
-let nomeProduto = document.getElementById('produto');
-let quantidade = document.getElementById('quatidade');
-alert(nomeProduto.value);
+let produto = document.getElementById('produto').value;
+let nomeProduto = produto.split('-')[0];
+let valorUnitario = produto.split('R$')[1];
+let quantidade = document.getElementById('quantidade');
+alert(nomeProduto);
+alert(valorUnitario);
 alert(quantidade.value);
+let preco = quantidade.value * valorUnitario;
+alert(preco);
 
 //calcular o preço, subtotal
 //adicionar no carrinho e subtotal
